f696a13a6fab27c8838126ed4c27f076a8336d12
P Sai Charan <papinenisaicharan@gmail.com>
2023-09-10 19:35:40 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -53,38 +53,38 @@ const Dashboard = () => {
     return (
         <div className="bg-gray-300 min-h-screen">
             <div className="px-6 ">
-                <div className="py-7 px-2 font-bold text-6xl text-white rounded-lg mb-4 mt5  bg-gray-700">
+                <div className="py-7 px-2 font-bold lg:text-6xl text-3xl lg:text-left text-center text-white rounded-lg mb-4 mt5  bg-gray-700">
                     AMPLIFY
                 </div>
             </div>
 
             <div className="px-6">
-                <div className="py-3 px-2 w-full font-semibold rounded-lg text-2xl mb-5 text-white bg-gray-700">
+                <div className="py-3 px-2 w-full font-semibold lg:text-left text-center rounded-lg text-2xl mb-5 text-white bg-gray-700">
                     Dashboard
                 </div>
             </div>
 
             <div className="px-6">
-                <div className="py-3 px-2 w-full font-semibold rounded-lg text-2xl mb-6 text-white bg-gray-700">
+                <div className="py-3 px-2 w-full font-semibold lg:text-left text-center rounded-lg text-2xl mb-6 text-white bg-gray-700">
                     Potential Leaders
                 </div>
             </div>
 
-            <div className="container mx-auto py-4 px-10 w-full">
+            <div className="container mx-auto lg:py-4 py-1 lg:px-10 px-1 lg:w-full lg:text-xl text-sm  ">
                 <table className="table-auto w-full bg-white border shadow-md rounded-md overflow-hidden">
                     <thead>
                         <tr>
-                            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('name')}>
+                            <th className=" px-1 lg:px-4 py-2 cursor-pointer" onClick={() => handleSort('name')}>
                                 Name {sortColumn === 'name' && (sortDirection === 'asc' ? '▲' : '▼')}
                             </th>
-                            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('base')}>
+                            <th className=" px-1 lg:px-4 py-2 cursor-pointer" onClick={() => handleSort('base')}>
                                 Base {sortColumn === 'base' && (sortDirection === 'asc' ? '▲' : '▼')}
                             </th>
-                            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('seniority')}>
+                            <th className=" px-1 lg:px-4 py-2 cursor-pointer" onClick={() => handleSort('seniority')}>
                                 Seniority {sortColumn === 'seniority' && (sortDirection === 'asc' ? '▲' : '▼')}
                             </th>
-                            <th className="px-4 py-2">Channel</th>
-                            <th className="px-4 py-2 cursor-pointer">
+                            <th className=" px-1 lg:px-4 py-2">Channel</th>
+                            <th className=" px-1 lg:px-4 py-2 cursor-pointer">
                                 Pictures
                             </th>
                         </tr>
@@ -92,17 +92,17 @@ const Dashboard = () => {
                     <tbody >
                         {data.map((item, index) => (
                             <tr key={index} className={index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'}>
-                                <td className="px-14 py-2">{item.name}</td>
-                                <td className="px-14 py-2">{item.base}</td>
-                                <td className="px-14 py-2">{item.seniority}</td>
-                                <td className="px-14 py-2">
+                                <td className="px-1 lg:px-14 py-2">{item.name}</td>
+                                <td className="px-1 lg:px-14 py-2">{item.base}</td>
+                                <td className="px-1 lg:px-14 py-2">{item.seniority}</td>
+                                <td className="px-1 lg:px-14 py-2">
                                     <select className="border p-1" defaultValue={item.channel}>
                                         <option value="1">FA Interested</option>
                                         <option value="2">Leader Identified</option>
                                         <option value="3">Eligible by Seniority</option>
                                     </select>
                                 </td>
-                                <td className="px-4 py-2">
+                                <td className="px-1 lg:px-14 py-2">
                                     <img src={item.picture} alt={item.name} className="rounded-full w-10 h-10" />
                                 </td>
                             </tr>
